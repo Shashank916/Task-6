a01adee6d740604ac3b790981e1aa387e6b552e3
Shashank K S <shashank.s2@mphasis.com>
2023-07-22 10:16:07 +0530

    Task-6

---
+++ Task-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2,4,6,8,10 };
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            double average = (double)sum / numbers.Length;
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+        }
+    }
+}
